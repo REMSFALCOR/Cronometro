86d6cce883bf719d69cffea355e0ade9f036deab
Richard Enrique Mareño Salvatierra <remsfalcor@gmail.com>
2024-09-09 02:24:53 +0200

    first commit

---
+++ Cronometro/Cronometro/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.IO;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Cronometro
+{
+    public partial class MainWindow : Window
+    {
+        private ArrayList _imageFiles;
+        DispatcherTimer DpT = new DispatcherTimer();
+        public int increSeg = 0;
+        Tiempo tp;
+
+        public MainWindow()
+        {
+            tp = new Tiempo();
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            DpT.Start();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            increSeg = 0;
+            MostraEnPantalla();
+            DpT.Stop();
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            DpT.IsEnabled = false;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //DpT.Interval = TimeSpan.FromSeconds(1);// para que realize intervalos de 1s en 1s
+            DpT.Tick += Incrementa;
+        }
+
+        private void Incrementa(object sender, EventArgs e)
+        {
+            MostraEnPantalla();
+            increSeg++;
+        }
+
+        private void MostraEnPantalla()
+        {
+            Hora.Text = tp.hora(increSeg);
+            Minutos.Text = tp.minuto(increSeg);
+            Segundos.Text = tp.segundo(increSeg);
+        }
+
+    }
+}
---
+++ Cronometro/Cronometro/Tiempo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cronometro
+{
+    internal class Tiempo: IHora, IMinuto, ISegundo
+    {
+        readonly int HorEnSeg = 3600;
+        readonly int MinEnSeg = 60;
+
+        /*Calcula cuantoas horas existen*/
+        public string hora(int ts)
+        {
+            int th = 0;
+            if (ts > HorEnSeg)
+                th = ts / HorEnSeg;
+            else
+                th = 0;
+            if (th < 10)
+                return "0" + th.ToString();
+            else
+                return th.ToString();
+        }
+
+        /*Calcula cuantos minutos exsiten*/
+        public string minuto(int ts)
+        {
+            int th=0,tm=0;
+
+            if (ts > HorEnSeg)
+                th = ts / HorEnSeg;
+            else
+                th = 0;
+            if (ts >= MinEnSeg)
+                tm= (ts - HorEnSeg * th) / 60;
+            else
+                tm= 0;
+            if (tm < 10)
+                return "0" + tm.ToString();
+            else
+                return tm.ToString();
+        }
+
+        /*Calcula los segundos que han pasado*/
+        public string segundo(int ts)
+        {
+            int th, tm;
+            if (ts > HorEnSeg)
+            {
+                th = ts / HorEnSeg;
+                ts = ts - HorEnSeg * th;
+            }
+            if (ts >= MinEnSeg)
+            {
+                tm = ts / MinEnSeg;
+                ts = ts - MinEnSeg * tm;
+            }
+
+            if (ts < 10)
+                return "0" + ts.ToString();
+            else
+                return ts.ToString();
+            //return ts;
+        }
+    }
+}
